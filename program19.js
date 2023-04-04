80936bbed0af9dffd0b460081366832716f050e3
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:40:52 +0500

    else if change marks

--- program19.js
+++ program19.js
@@ -24,7 +24,7 @@ function program19(Physics,Chemistry,Biology,Mathmatical,computer) {
     console.log("Grade C=",marks);
    }
    else if (per>=60) {
-    console.log("Grade D=");
+    console.log("Grade D=",marks);
    }
    else if (per>=40) {
     console.log("Grage E=",marks);
