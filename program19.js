7df2762adb926d360ae018224767cfe1e88fa76e
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:34:16 +0500

    Percentage >= 40% : Grade E

--- program19.js
+++ program19.js
@@ -3,6 +3,7 @@
 // Percentage >= 80% : Grade B
 // Percentage >= 70% : Grade C
 // Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
 function program19(Physics,Chemistry,Biology,Mathmatical,computer) {
     var a=+prompt("Enter a Physics Subject Number");
     var b=+prompt("Enter a Chemistry Subject Number");
