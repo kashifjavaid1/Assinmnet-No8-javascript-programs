521a1d12c3d003f5a2712b9a7932bd1690789de0
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:21:46 +0500

    Write a js program to input any alphabet and check whether it is vowel or consonant.

--- program8.js
+++ program8.js
@@ -1,3 +1,4 @@
+// Write a js program to input any alphabet and check whether it is vowel or consonant.
 function program8(a) {
     var enter = prompt("User Enter Any Charachter ");
     if (enter == 'a' || enter == 'e' || enter == 'i' || enter == 'o' || enter == 'u') {
