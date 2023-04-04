b26e971fa31939efa239edcbf06ddb84fc460b5f
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:00:23 +0500

    1 Program
    Maximum or Minimum Numbers find

---
+++ program1.js
@@ -0,0 +1,17 @@
+
+console.log("maximum two Numbers=");
+function program1(max, max1) {
+    var a = +prompt("User Enter First Number ");
+    var b = +prompt("User Enter Second Number");
+    if (a>b) {
+       console.log("Maximum Number A=",a); 
+    }
+    else if (b>a) {
+        console.log("maximum Number B=",b);
+    }
+    else{
+        console.log("Both Number are equal");
+
+    }
+}
+program1()
