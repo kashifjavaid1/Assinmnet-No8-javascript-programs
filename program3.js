8a5ad7b3fc6cec7ada45dd9a614f242fd9a5692c
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:09:08 +0500

    Program No3
    Positive and negative number

---
+++ program3.js
@@ -0,0 +1,14 @@
+
+function program3(a) {
+    var a=+prompt("User Enter a Number");
+    if (a==0) {
+        console.log("Number is Zero",a);
+    }
+    else if (a>0) {
+        console.log("Number is Positive",a);
+    }
+    else if (a<0) {
+        console.log("Number is Negative",a);
+    }
+}
+program3()
