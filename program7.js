cc3c3598b41943ba1d5ce91ccf10f162bfd86ead
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:19:58 +0500

    programno7

---
+++ program7.js
@@ -0,0 +1,10 @@
+function program7(a) {
+    var a=prompt("User Enter alphabet");
+   if (a>=0) {
+    console.log("User Enter a Number=",a);
+   }
+   else{
+    console.log("User Enter a Alphabets=",a);
+   }
+}
+program7()
