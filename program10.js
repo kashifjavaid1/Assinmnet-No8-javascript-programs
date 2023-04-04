80838de0573f277e27ee9a99b8950b36ffca982a
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:24:30 +0500

    program No.10

---
+++ program10.js
@@ -0,0 +1,12 @@
+ 
+ function program10(a) {
+    var user=prompt("User Enter any Charachter");
+    if (user == 'a' || user == 'b' || user == 'c' || user == 'd' || user == 'e' || user == 'f' || user == 'g' || user == 'h' || user == 'i' || user == 'j' || user == 'k' || user == 'l' || user == 'm' || user == 'n' || user == 'o' || user == "p" || user == 'q' || user == 'r' || user == 's' || user == 't' || user == 'u' || user == "v" || user == 'w' || user == 'x' || user == "y" || user == 'z')    {
+        console.log("User Enter Lowercase");
+      }
+    else if (user == 'A' || user == 'B' || user == 'C' || user == 'D' || user == 'E' || user == 'F' || user == 'G' || user == 'H' || user == 'I' || user == 'J' || user == 'K' || user == 'L' || user == 'M' || user == 'N' || user == 'O' || user == "P" || user == 'Q' || user == 'R' || user == 'S' || user == 'T' || user == 'U' || user == "V" || user == 'W' || user == 'X' || user == "Y" || user == 'Z')    {
+            console.log("User Enter Uppercase");
+          }
+    }
+ 
+ program10()
