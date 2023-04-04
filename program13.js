fcf4d83ab274d63d8e72c2e3c8f058cac2f9f4c7
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:28:38 +0500

    program No13

---
+++ program13.js
@@ -0,0 +1,11 @@
+function program13(a,b,c,d,f) {
+    var a=+50;
+    var b=+40;
+    var c=+45;
+    var d=+30;
+    var e=+40;
+    var result=a+b+c+d+e;
+    console.log(a,b,c,d,e);
+    console.log("Total ",result);
+}
+program13()
