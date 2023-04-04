dd0be7e68abd88f7cd11a57a66e75330bff8313c
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:18:09 +0500

    programNO.6

---
+++ program6.js
@@ -0,0 +1,10 @@
+ function program6(a) {
+    var a=+prompt("User Enter 4 Year's days");
+    if (a%4==0) {
+        console.log("Leap Year",a);
+    }
+    else{
+        console.log("Not Leap Year",a);
+    }
+ }
+ program6()
