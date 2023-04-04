974cd1d0649b284213f6a9a08dff0aab56f8726a
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:29:51 +0500

    program No18

---
+++ program18.js
@@ -0,0 +1,20 @@
+function program18() {
+    var sellingprice =+prompt("Enter a Selling Price");
+    var costprice =+prompt("Enter a Cost Price");
+  
+    if (sellingprice > costprice) {
+      console.log("Profit");
+    } else if (costprice > sellingprice) {
+      console.log("Loss");
+    } else {
+      console.log("No Profit No Loss");
+    }
+  }
+  
+  program18();
+  
+  
+  
+  
+  
+  
