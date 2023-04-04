4d94050e17ce06afddfccd167d51e74aeb2d5e18
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:16:30 +0500

    program no5

---
+++ programno5.js
@@ -0,0 +1,12 @@
+
+
+function abc(even,odd) {
+    var evennum=+prompt("Enter Any Number");
+if (evennum%2==0) {
+    console.log("User Enter even Number",evennum);
+}
+else {
+    console.log("User Enter odd Number",evennum);
+}
+}
+abc()
