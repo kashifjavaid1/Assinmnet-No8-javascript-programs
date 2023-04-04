6fa6faad64de671be0ecc60fada20bb0bc20450c
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:36:37 +0500

    Grade D change

--- program19.js
+++ program19.js
@@ -2,7 +2,7 @@
 // Percentage >= 90% : Grade A ?
 // Percentage >= 80% : Grade B?
 // Percentage >= 70% : Grade C?
-// Percentage >= 60% : Grade D
+// Percentage >= 60% : Grade D ?
 // Percentage >= 40% : Grade E
 // Percentage < 40% : Grade F
 function program19(Physics,Chemistry,Biology,Mathmatical,computer) {
