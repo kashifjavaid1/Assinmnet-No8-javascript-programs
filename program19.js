f028ea30f79271fbc9156e270fcda59edddb2714
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:31:30 +0500

    program No.19

---
+++ program19.js
@@ -0,0 +1,32 @@
+function program19(Physics,Chemistry,Biology,Mathmatical,computer) {
+    var a=+prompt("Enter a Physics Subject Number");
+    var b=+prompt("Enter a Chemistry Subject Number");
+    var c=+prompt("Enter a  Biology Subject Number");
+    var d=+prompt("Enter a Mathmatical Subject Number");
+    var e=+prompt("Enter a Computer Subject Number");
+    var marks=a+b+c+d+e;
+   var total=500;
+   var per= marks/total*100;
+   if (per>=90) {
+    console.log("Grade A=",marks);
+   }
+   else if (per>=80) {
+    console.log("Grage B=",marks);
+   }
+   else if (per>=70) {
+    console.log("Grade C=",marks);
+   }
+   else if (per>=60) {
+    console.log("Grade D=",marks);
+   }
+   else if (per>=40) {
+    console.log("Grage E=",marks);
+   }
+   else if (per<40) {
+    console.log("Grade F=",marks);
+   }
+
+
+
+}
+program19()
