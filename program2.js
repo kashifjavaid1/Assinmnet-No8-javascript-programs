696099c9a1ac0c583e119b2fd399c766a39ee72f
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:06:04 +0500

    Program 3

---
+++ program2.js
@@ -0,0 +1,20 @@
+
+console.log("maximum three Numbers=");
+function program2() {
+    var a=+prompt("User Enter First Number");
+    var b=+prompt("User Enter Second Number");
+    var c=+prompt("User Enter Third Number");
+    if (a>b) {
+        console.log("Maximum Number A=",a);
+    }
+    else if (b>c) {
+        console.log("Maximum Number B=",b);
+    }
+    else if (c>a) {
+        console.log("Maxmimu Number C=",c);
+    }
+    else{
+        console.log("Both Number are equal");
+    }
+}
+program2()
