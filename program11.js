8f2e034d0a3f339475c047a2d2d128525b8f0acb
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:26:14 +0500

    program No11

---
+++ program11.js
@@ -0,0 +1,28 @@
+function program11(days) {
+    var days=+prompt("Enter a Number in week");
+    if (days==1 ) {
+        console.log("First day in week Monday");
+    }
+    else if (days==2) {
+        console.log("Second day in week Tuseday");
+    }
+    else if (days==3) {
+        console.log("Third day in week Wednesday");
+    }
+    else if (days==4) {
+        console.log("Fourth day in week Thuresday");
+    }
+   else if (days==5) {
+        console.log("Fifth day in week Friday");
+    }
+    else if (days==6) {
+        console.log("Sixth day in week Saturday");
+    }
+   else if (days==7) {
+        console.log("Seventh day in week Sunday");
+    }
+    else {
+        console.log(" Just seven days in week");
+    }
+}
+program11()
