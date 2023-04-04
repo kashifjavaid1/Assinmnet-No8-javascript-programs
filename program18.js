980a3410bae9288eb545eed606124703f10296e5
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:30:21 +0500

    Write a js program to calculate profit or loss.

--- program18.js
+++ program18.js
@@ -1,3 +1,4 @@
+// Write a js program to calculate profit or loss.
 function program18() {
     var sellingprice =+prompt("Enter a Selling Price");
     var costprice =+prompt("Enter a Cost Price");
