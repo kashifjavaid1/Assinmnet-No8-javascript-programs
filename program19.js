7830621877aaf4b55db061186e32f79537d96f2d
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:43:14 +0500

    variblae c

--- program19.js
+++ program19.js
@@ -8,7 +8,7 @@
 function program19(Physics,Chemistry,Biology,Mathmatical,computer) {
     var a=+prompt("Enter a Physics Subject Number");
     var b=+prompt("Enter a Chemistry Subject Number");
-    // var c=+prompt("Enter a  Biology Subject Number");
+    var c=+prompt("Enter a  Biology Subject Number");
     // var d=+prompt("Enter a Mathmatical Subject Number");
     // var e=+prompt("Enter a Computer Subject Number");
     // var marks=a+b+c+d+e;
