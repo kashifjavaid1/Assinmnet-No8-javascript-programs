7c2eae9d696b89ae069cf5c0606f364366e2a424
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-04 23:27:24 +0500

    program No12

---
+++ program12.js
@@ -0,0 +1,40 @@
+function month() {
+    var month = +prompt("Enter a any Month Number");
+    if (month == 1) {
+        console.log("firsth month   january in  31 days ");
+    }
+    else if (month == 2) {
+        console.log("Seconde  month   February  in  28 days");
+    }
+    else if (month == 3) {
+        console.log("Third  month   March  in  31 days");
+    } 
+    else if (month == 4) {
+        console.log("Fourth   month   APril  in  30 days");
+    } 
+    
+    else if (month == 5) {
+        console.log("Fifth   month   May   in  31 days");
+    }
+    else if (month == 6) {
+        console.log("Sixth  month   June  in  30 days");
+    } else if (month == 7) {
+        console.log("Seventh  month   July  in  31  days");
+    }
+    else if (month==8) {
+        console.log("Eight   month   August  in  31 days");
+    }
+    else if (month==9) {
+        console.log("Ninth  month   September   in  30 days");
+    }
+    else if (month==10) {
+        console.log("Tenth  month   October  in  31 days");
+    }
+    else if (month==11) {
+        console.log("Eleventh  month   November  in  30 days");
+    }
+    else if (month==12) {
+        console.log("twelfth month   February  in  31 days");
+    }
+}
+month()
